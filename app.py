757ab5cbbc67be76852f1a6db6e50ad63748c05a
Bhavya Shah <shahbhavyan12@gmail.com>
2021-06-07 16:38:53 +0530

    commiting_backend

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from project import app
 import os
+from flask import Flask
+
+#app = Flask(__name__)
+
+print(app)
+
+@app.route("/")
+def hello_world():
+    return("Hello World")
+
+
 if __name__=='__main__':
     app.run(debug=True,
             host="0.0.0.0",
